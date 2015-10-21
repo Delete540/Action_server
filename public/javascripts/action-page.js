0ab56aea6072d063c808d3183cf9630391cbe912
delete540 <delete540@gmail.com>
2015-10-21 16:13:32 +0800

    Fix hamburger button and header title

--- public/javascripts/action-page.js
+++ public/javascripts/action-page.js
@@ -10,7 +10,13 @@ $(function(){
 
 function actionScrollEvent(){
     $(window).scroll(function () {
-    
+        if ($(window).scrollTop() > ($('.action-header-nav').height()/2)) {
+            $('.action-header-title').addClass('action-active-hte').removeClass('action-active-htd');
+        }else{
+            $('.action-header-title').addClass('action-active-htd').removeClass('action-active-hte');
+        }
+        
+        
         if ($(window).scrollTop() > ($('.action-header').height()-$('.action-header-nav').height())) {
             $('.action-header-nav').addClass('action-box-shadow-default action-active-bcd').removeClass('action-box-shadow-none action-style-color-0');
             
