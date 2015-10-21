4f152fcc0981b01777ca9802f094586a52dcf193
delete540 <delete540@gmail.com>
2015-10-21 17:32:05 +0800

    Fix header button and some bugs

--- public/javascripts/action-page.js
+++ public/javascripts/action-page.js
@@ -4,27 +4,28 @@ $(function(){
    
    $(window).bind('resize load', function() {
        actionScrollEvent();
+       console.log($(window).scrollTop());
+        $(window).scroll(function () {
+            actionScrollEvent();
+        });
    });
    
 })
 
 function actionScrollEvent(){
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > ($('.action-header-nav').height()/2)) {
-            $('.action-header-title').addClass('action-active-hte').removeClass('action-active-htd');
-        }else{
-            $('.action-header-title').addClass('action-active-htd').removeClass('action-active-hte');
-        }
+    
+    if ($(window).scrollTop() > ($('.action-header-nav').height()*.4)) {           
+        $('.action-header-title').addClass('action-active-hte').removeClass('action-active-htd');
+    }else{
+        $('.action-header-title').addClass('action-active-htd').removeClass('action-active-hte');
+    }
+    
+    
+    if ($(window).scrollTop() > ($('.action-header').height()-$('.action-header-nav').height())) {
+        $('.action-header-nav').addClass('action-box-shadow-default action-active-bcd').removeClass('action-box-shadow-none action-style-color-0');
+    }else {
+        $('.action-header-nav').addClass('action-box-shadow-none action-style-color-0').removeClass('action-box-shadow-default action-active-bcd');
         
-        
-        if ($(window).scrollTop() > ($('.action-header').height()-$('.action-header-nav').height())) {
-            $('.action-header-nav').addClass('action-box-shadow-default action-active-bcd').removeClass('action-box-shadow-none action-style-color-0');
-            
-        }else {
-            $('.action-header-nav').addClass('action-box-shadow-none action-style-color-0').removeClass('action-box-shadow-default action-active-bcd');
-            
-        }
-        
-    });
+    }
     
 }
