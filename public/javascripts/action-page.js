52e2e52b5336af8f9357e56ef1b7d542341088d2
delete540 <delete540@gmail.com>
2015-10-23 21:20:11 +0800

    Add button tooltip

--- public/javascripts/action-page.js
+++ public/javascripts/action-page.js
@@ -1,6 +1,7 @@
 $(function(){
    //var actionBtnIcons=new SVGMorpheus('#icon'); 
-   
+    
+   actionDesktopButtonHoverEvent();
    
    $(window).bind('resize load', function() {
        actionScrollEvent();
@@ -22,13 +23,31 @@ $(function(){
    
 })
 
+
 function actionHeaderButtonEvent(){
+    
+    $('.action-hamburger-button').click(function(){
+        //event.stopPropagation();
+        $('body').css('overflow','hidden').append('<div class="action-mask action-mask-black"></div>');
+        $('.action-drag-target').addClass('action-active-width-full').removeClass('action-active-width-0');
+        $('.action-side-nav').addClass('action-active-x-0').removeClass('action-active-x-left').css('overflow','auto');
+        //$('.action-mask-black').addClass('action-active-opacity-1').removeClass('action-active-opacity-0');
+    });
+    
+    $('.action-drag-target').click(function(){
+        $('body').removeAttr('style');
+        $('.action-drag-target').addClass('action-active-width-0').removeClass('action-active-width-full');
+        $('.action-side-nav').addClass('action-active-x-left').removeClass('action-active-x-0');
+        //$('.action-mask-black').addClass('action-active-opacity-0').removeClass('action-active-opacity-1');
+        $('.action-mask-black').remove();
+    });
+    
     $('.action-header-mobile-add-button').click(function(){
         $('.action-mobile-nav-dropdown').css({
             'margin-right':'-32px'
         });
-        
     });
+    
 }
 
 function actionScrollEvent(){
@@ -58,14 +77,29 @@ function actionMobileSideNavEvent(){
         .css('width',($('.action-mobile-side-search-page-header nav .nav-wrapper').width()-$('.action-mobile-side-search-page-header nav button').width()-36));
     
     $(' .action-header-nav-right-menu .action-header-mobile-search-button').click(function(){
-        $('.action-mobile-side-search-page').addClass('action-active-msne').removeClass('action-active-msnd');
+        $('.action-mobile-side-search-page').addClass('action-active-x-0').removeClass('action-active-x-right');
         $('body').css('overflow','hidden');
         $('.action-mobile-side-search-page').css('overflow','auto');
     });
     $('.action-mobile-side-search-page-header nav .nav-wrapper button.action-back-button').click(function(){
-        $('.action-mobile-side-search-page').addClass('action-active-msnd').removeClass('action-active-msne');
-        $('body').css('overflow','auto');
+        $('.action-mobile-side-search-page').addClass('action-active-x-right').removeClass('action-active-x-0');
+        $('body').removeAttr('style');
     });
     
     
+}
+
+function actionDesktopButtonHoverEvent(){
+    $('.action-header-nav-right-menu.action-d-down-hide button').hover(function(){
+         $('.action-header-nav-right-menu').append('<div class="action-tooltip black "><span class="material-icons">arrow_drop_up</span><span></span></div>')
+        $('.action-tooltip').show().css('right',($(window).width()-$(this).offset().left-$('.action-tooltip').width()-6));
+        $('.action-tooltip span:eq(1)').text($(this).attr('action-tooltip'));
+        //console.log($(window).width()-$(this).offset().left);
+    },function(){
+        
+        $('.action-tooltip').hide();
+        $('.action-tooltip span:eq(1)').text('');
+        $('.action-tooltip').remove();
+        
+    });
 }
