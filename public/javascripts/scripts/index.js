a9aa8bf38d3a4f85f8ea554c82583db5ce1f301a
delete540 <delete540@gmail.com>
2016-07-06 01:59:15 +0800

    fix init angular moudle and add angular-route

---
+++ public/javascripts/scripts/index.js
@@ -0,0 +1,16 @@
+//Angular & Angular Material conifg
+angular.module('Action', ['ngRoute'])
+// angular.module('ActionApp', ['ngRoute','ngMaterial','ngMdIcons'])
+//Material config
+// .config(function($mdThemingProvider) {
+//   // $mdThemingProvider.theme('defaultTheme')
+//   //   .primaryPalette('blue');
+// })
+
+// .controller('ActionEventController',function ($scope, $timeout, $mdSidenav, $mdUtil, $log){
+  
+// })
+
+//   .controller('ActionHeaderController',function(){})
+  
+//   .controller('ActionHeaderNavController',function(){})
