e5a46032c23a4aeb6b6f3a010f75d6c618ac8067
7274 <7274@rdc.hand-china.com>
2016-09-08 15:51:12 +0800

    change header

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 
 var config = {
-  path:"",//root of the project dir;
+  path:"",//root of the project dir;  
   db:"mongodb://localhost/action",
   secret:'action',
 };
 
 if(process.env.NODE_ENV=='development'){
-  config.db='mongodb://169.254.4.101:27017/action';
+  config.db='mongodb://192.168.15.133:27017/action';
 }
 config.path=process.cwd();
-
+  
 module.exports = config;
