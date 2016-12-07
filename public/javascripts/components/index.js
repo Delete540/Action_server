945917b7e4c57a495aacf63ee9b5ff65c1ce81c1
李俊 <7274@rdc.hand-china.com>
2016-12-07 20:32:53 +0800

    add click event/sidenav event

--- public/javascripts/components/index.js
+++ public/javascripts/components/index.js
@@ -9,114 +9,164 @@
 
 var Matcha = {};
 
-(function(window){
+(function(window) {
 
-  var $doc;
+    var $doc;
 
-  if(window.Package){
-    Matcha = {};
-  } else {
-    window.Matcha = {};
-  }
-
-  
-  if(window.document){
-    $doc = window.document;
-  }
-
-  Matcha = {
-    fn:function(elements,selector,callback){
-
-      this.elements = elements;
-      this.selector = selector;
-      if(callback){
-        this.callback = callback(this);
-      }
-
-      return this;
-    },
-
-    init: function(){
-
-      var arg=[],
-          props;
-      
-      if(arguments[0]===undefined || typeof arguments[0]!='object' || arguments[0].length<=0){
-        return;
-      }
-
-      arg[0] = arguments[0];
-
-      if(arguments[1] && typeof arguments[1] ==='string'){
-        arg[1] = arguments[1];
-      }
-
-      if(arguments[2] && typeof arguments[2] ==='function'){
-        arg[2] = arguments[2];
-      }
-
-      Matcha.fn.prototype = {
-        eq: function(num){
-          var object;
-          object = (new Matcha.fn(arg[0][num],arg[1]));
-          return object;
-        },
-        addClass: function(className){
-          for(var i=0;i<this.elements.lenght;i++){
-            this.elements[i].classList.add(className);
-          }
-          return this;
-        },
-        removeClass: function(className){
-          for(var i=0;i<this.elements.lenght;i++){
-            this.elements[i].classList.remove(className);
-          }
-        },
-        attr: function(attr1,attr2){
-          var object = new Matcha.fn(arg[0][0],arg[1]).elements;
-          if(attr2){
-            object.setAttribute(attr1,attr2);
-          }
-          return object.getAttribute(attr1);
-        },
-        css: function(object){
-          
-          if(object && typeof object == 'object'){
-            for(var i=0;i<this.elements.length;i++){
-              var style ='';
-              for(var key in object){
-                style =style+key+':'+object[key]+';';
-              }
-              this.elements[i].setAttribute('style',style);
-            }
-          }
-          return this; 
-        },
-        click: function(callback){
-          if(callback && typeof callback === 'function'){
-            document.addEventListener('DOMContentLoaded', function(e){
-
-              document.body.addEventListener('mousedown', function(e){
-                callback();
-              }, false);
-
-            }, false);
-            
-          }
-        }
-      };
-
-      props = new Matcha.fn(arg[0],arg[1]);
-
-      return props;
-    },
-
-    find: function(selector){
-      return Matcha.init($doc.querySelectorAll(selector),selector);
+    if (window.Package) {
+        Matcha = {};
+    } else {
+        window.Matcha = {};
     }
 
-  };
+
+    if (window.document) {
+        $doc = window.document;
+    }
+
+    Matcha = {
+        fn: function(elements, selector, callback) {
+
+            this.elements = elements;
+            this.selector = selector;
+            if (callback) {
+                this.callback = callback(this);
+            }
+
+            return this;
+        },
+
+        init: function() {
+
+            var arg = [],
+                props;
+
+            if (arguments[0] === undefined || typeof arguments[0] != 'object' || arguments[0].length <= 0) {
+                return;
+            }
+
+            arg[0] = arguments[0];
+
+            if (arguments[1] && typeof arguments[1] === 'string') {
+                arg[1] = arguments[1];
+            }
+
+            if (arguments[2] && typeof arguments[2] === 'function') {
+                arg[2] = arguments[2];
+            }
+
+            Matcha.fn.prototype = {
+                eq: function(num) {
+                    var object;
+                    object = (new Matcha.fn(arg[0][num], arg[1]));
+                    return object;
+                },
+                addClass: function(className) {
+                    for (var i = 0; i < this.elements.length; i++) {
+                        this.elements[i].classList.add(className);
+                    }
+                    return this;
+                },
+                removeClass: function(className) {
+                    for (var i = 0; i < this.elements.length; i++) {
+                        this.elements[i].classList.remove(className);
+                    }
+                },
+                attr: function(attr1, attr2) {
+                    var object = new Matcha.fn(arg[0][0], arg[1]).elements;
+                    if (attr2) {
+                        object.setAttribute(attr1, attr2);
+                    }
+                    return object.getAttribute(attr1);
+                },
+                css: function(object) {
+
+                    if (object && typeof object == 'object') {
+                        for (var i = 0; i < this.elements.length; i++) {
+                            var style = '';
+                            for (var key in object) {
+                                style = style + key + ':' + object[key] + ';';
+                            }
+                            this.elements[i].setAttribute('style', style);
+                        }
+                    }
+                    return this;
+                },
+                click: function(callback) {
+                    var elements = this.elements;
+                    console.log('a');
+                    function clickUtils(e) {
+                        console.log('a');
+                        var event = e ? e : window.event,
+                            target = e.target || e.srcElement,
+                            flag = false;
+                        for (var i = 0; i < elements.length; i++) {
+
+                            if (target.className.indexOf(elements[i].className) != -1) {
+                                flag = true;
+                            } else if (target.parentElement !== null && target.parentElement.className.indexOf(elements[i].className) != -1) {
+                                flag = true;
+                                target = target.parentElement;
+                            }
+                        }
+                        if (flag) {
+                            callback(event, target);
+                            Matcha.find('.side-nav').elements[0].innerHTML = Matcha.find('body').elements[0].className;
+                        }
+                    }
+                    $doc.addEventListener('touchstart', function(e) {
+                      clickUtils(e);
+                    }, false);
+
+                    $doc.addEventListener('click', function(e) {
+                      clickUtils(e);
+                    }, false);
+                }
+            };
+
+            props = new Matcha.fn(arg[0], arg[1]);
+
+            return props;
+        },
+
+        find: function(selector) {
+            return Matcha.init($doc.querySelectorAll(selector), selector);
+        },
+
+        createMask: function() {
+            var mask = document.createElement('div');
+            mask.className = 'action-mask mask';
+            $doc.body.appendChild(mask);
+        },
+
+        stopEventBubbling: function(event) {
+            if (event) {
+                if (event.stopPropagation) {
+                    event.stopPropagation();
+                }
+                else {
+                    event.cancelBubble = true;
+                }
+            }
+        },
+        isFunction: function(func) {
+            return func ? ((typeof func === 'function') ? true : false) : false;
+        },
+        initEvent: function(callback) {
+            $doc.addEventListener('DOMContentLoaded', function() {
+                if (Matcha.isFunction(callback)) {
+                    callback();
+                }
+            }, false);
+        }
+
+    };
+
+    Matcha.initEvent(function() {
+        Matcha.createMask();
+    });
+
+
 
 })(window);
-
 
--- public/javascripts/components/sidenav.js
+++ public/javascripts/components/sidenav.js
@@ -1,12 +1,33 @@
 //
-(function (window) {
-  var element,targetSidenav;
-  document.addEventListener('DOMContentLoaded', function(){
-    element=Matcha.find('#actionHeaderNav .action-hamburg');
-    element.click(function(e){
-      console.log('a');
-    });
-  }, false);
 
+(function(window) {
+    
+        actionLeftSideNavEvent = function() {
+          
+          var hamburg = Matcha.find('#actionHeaderNav .action-hamburg'),
+            targetSidenav = Matcha.find('div.action-left-side'),
+            mask = Matcha.find('.action-mask.mask'),
+            body = Matcha.find('body');
+
+            hamburg.click(function() {
+                targetSidenav.removeClass('hide');
+                mask.addClass('show');
+                body.addClass('hide-scroll');
+
+            });
+            targetSidenav.click(function(e) {
+                Matcha.stopEventBubbling(e);
+            });
+            mask.click(function(e) {
+                targetSidenav.addClass('hide');
+                mask.removeClass('show');
+                body.removeClass('hide-scroll');
+
+            });
+        };
+    Matcha.initEvent(function() {
+        actionLeftSideNavEvent();
+    });
 
 })(window);
+
