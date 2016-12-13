9ab24937ed7eb199d122f409eef494fc19a20909
李俊 <7274@rdc.hand-china.com>
2016-12-13 14:05:49 +0800

    repair small screen bug

--- public/javascripts/components/headerNav.js
+++ public/javascripts/components/headerNav.js
@@ -10,11 +10,7 @@
       $docScrollTop = $doc.body.scrollTop,
       navTitle = Matcha.find('#actionNavCenterTitle').elements[0];
       userPhoto = Matcha.find('#actionNavCreateUserPhoto img').elements[0];
-      /*var card0 = {};
-      card0.element = cards.elements[0];
-      card0.top = baseHeight + cards.elements[0].offsetTop;
-      card0.bottom = card0.top + cards.elements[0].offsetHeight;
-      elements.push(card0);*/
+
       for (var i = 0; i < cards.elements.length; i++) {
         var card = {};
         card.element = cards.elements[i];
@@ -24,9 +20,8 @@
       }
       if (elements && elements.length > 0) {
         for (var j = 0; j < elements.length;j++){
-          console.log($docScrollTop);
-          if($docScrollTop>baseHeight && $docScrollTop<=elements[j].bottom && $docScrollTop>=elements[j].top){
-            //console.log(Matcha.find('#actionHeaderNav container h5'));
+          if(document.body.offsetWidth >600 && $docScrollTop>baseHeight && $docScrollTop<=elements[j].bottom && $docScrollTop>=elements[j].top){
+
             navTitle.innerHTML = elements[j].element.getAttribute('data-action-title');
             userPhoto.src = elements[j].element.getAttribute('data-photo');
             userPhoto.parentNode.classList.add('grow');
